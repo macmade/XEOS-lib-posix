dcd346af91ce7bfee9bf2f67c6a29cd12a52ea85
Jean-David Gadina <macmade@xs-labs.com>
2013-08-01 17:31:58 +0200

    Added umbrella header files.

---
+++ include/posix.h
@@ -0,0 +1,162 @@
+/*******************************************************************************
+ * XEOS - X86 Experimental Operating System
+ * 
+ * Copyright (c) 2010-2013, Jean-David Gadina - www.xs-labs.com
+ * All rights reserved.
+ * 
+ * XEOS Software License - Version 1.0 - December 21, 2012
+ * 
+ * Permission is hereby granted, free of charge, to any person or organisation
+ * obtaining a copy of the software and accompanying documentation covered by
+ * this license (the "Software") to deal in the Software, with or without
+ * modification, without restriction, including without limitation the rights
+ * to use, execute, display, copy, reproduce, transmit, publish, distribute,
+ * modify, merge, prepare derivative works of the Software, and to permit
+ * third-parties to whom the Software is furnished to do so, all subject to the
+ * following conditions:
+ * 
+ *      1.  Redistributions of source code, in whole or in part, must retain the
+ *          above copyright notice and this entire statement, including the
+ *          above license grant, this restriction and the following disclaimer.
+ * 
+ *      2.  Redistributions in binary form must reproduce the above copyright
+ *          notice and this entire statement, including the above license grant,
+ *          this restriction and the following disclaimer in the documentation
+ *          and/or other materials provided with the distribution, unless the
+ *          Software is distributed by the copyright owner as a library.
+ *          A "library" means a collection of software functions and/or data
+ *          prepared so as to be conveniently linked with application programs
+ *          (which use some of those functions and data) to form executables.
+ * 
+ *      3.  The Software, or any substancial portion of the Software shall not
+ *          be combined, included, derived, or linked (statically or
+ *          dynamically) with software or libraries licensed under the terms
+ *          of any GNU software license, including, but not limited to, the GNU
+ *          General Public License (GNU/GPL) or the GNU Lesser General Public
+ *          License (GNU/LGPL).
+ * 
+ *      4.  All advertising materials mentioning features or use of this
+ *          software must display an acknowledgement stating that the product
+ *          includes software developed by the copyright owner.
+ * 
+ *      5.  Neither the name of the copyright owner nor the names of its
+ *          contributors may be used to endorse or promote products derived from
+ *          this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE, TITLE AND NON-INFRINGEMENT ARE DISCLAIMED.
+ * 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR ANYONE DISTRIBUTING
+ * THE SOFTWARE BE LIABLE FOR ANY CLAIM, DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN ACTION OF CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/* $Id$ */
+
+#ifndef __LIBPOSIX_H__
+#define __LIBPOSIX_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#include <aio.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <complex.h>
+#include <cpio.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fenv.h>
+#include <float.h>
+#include <fmtmsg.h>
+#include <fnmatch.h>
+#include <ftw.h>
+#include <glob.h>
+#include <grp.h>
+#include <iconv.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <langinfo.h>
+#include <libgen.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <monetary.h>
+#include <mqueue.h>
+#include <ndbm.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <nl_types.h>
+#include <poll.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <regex.h>
+#include <sched.h>
+#include <search.h>
+#include <semaphore.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <spawn.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <stropts.h>
+#include <sys/ipc.h>
+#include <sys/mman.h>
+#include <sys/msg.h>
+#include <sys/resource.h>
+#include <sys/select.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/statvfs.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/un.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <syslog.h>
+#include <tar.h>
+#include <termios.h>
+#include <tgmath.h>
+#include <time.h>
+#include <trace.h>
+#include <ulimit.h>
+#include <unistd.h>
+#include <utime.h>
+#include <utmpx.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <wordexp.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBPOSIX_H__ */
